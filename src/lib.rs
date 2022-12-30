eef25c0426cbc9cbf04dfa0638b4e8f6fbdabed2
NutiNaguti <derzhavin.danila@icloud.com>
2022-12-30 13:29:18 +0400

    dev in progress

--- src/lib.rs
+++ src/lib.rs
@@ -111,9 +111,9 @@ impl ERC20 {
     }
 
     pub fn mint(&mut self, to: AccountId, value: U128) {
-        log!("Mint!");
+        // log!("Mint!");
         log!("key exist: {}", self.balance.contains_key(&to));
-        log!("Balance: {}", self.balance.get(&to).unwrap());
+        log!("Balance: {:?}", self.balance.get(&to));
         if let false = self.balance.contains_key(&to) {
             self.balance.insert(to.clone(), 0);
         }
